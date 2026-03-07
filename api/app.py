b1460304bdb56ad35f5fa37db65a2f00d37d85fb
kaustubh-aft <you@example.com>
2026-03-07 14:16:19 -0500

    Fixed paths and configuration for Vercel deployment

--- api/app.py
+++ api/app.py
@@ -1,29 +1,27 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 import subprocess
+import os
 
 app = FastAPI()
 
+# Get the path to the current file's directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Serve the interactive webpage
 @app.get("/")
 def homepage():
-    return FileResponse("index.html")
+    return FileResponse(os.path.join(BASE__DIR, "index.html"))
 
-
-# API endpoint that runs your python script
 @app.get("/api/band")
 def generate_band(city: str, pet: str):
-
+    script_path = os.path.join(BASE_DIR, "day1.py")
     process = subprocess.Popen(
-        ["python", "day1.py"],
+        ["python3", script_path],
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True
     )
-
     # feed inputs to the script
     output, error = process.communicate(f"{city}\n{pet}\n")
-
     return {"result": output}
